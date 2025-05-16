8fc08194891d799c27c3a16bc937f274d697c3df
birke12 <biirkegaard@gmail.com>
2025-05-16 10:50:38 +0200

    gowala_to_desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import IntroCard from "../components/introCards/IntroCards";
+/* import Products from "../components/products/Products"; */
 import SliderHeader from "../components/sliderHeader/SliderHeader";
 
 const Home = () => {
-    return (
-        <section>
-<SliderHeader />
-<IntroCard />
-        </section>
-    )
-}
+  return (
+    <section>
+      <SliderHeader />
+      <IntroCard />
+      {/* <Products /> */}
+    </section>
+  );
+};
 
 export default Home;
